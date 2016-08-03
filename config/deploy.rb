09dd3f246f0e8667fdfaaf39640165b6600b2599
Louise Crow <louise.crow@gmail.com>
2016-08-03 21:27:44 +0100

    Add test.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, '/srv/www/planningalerts'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/general.yml'
+append :linked_files, 'config/database.yml', 'config/general.yml', 'config/test.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
