624b31493285de254d58c8e463b2ec7646527dd2
Louise Crow <louise.crow@gmail.com>
2016-08-03 19:30:20 +0100

    New cap config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,81 +1,36 @@
-require 'new_relic/recipes'
-require 'bundler/capistrano'
-require 'rvm/capistrano'
-require 'rvm/capistrano/alias_and_wrapp'
-require 'delayed/recipes'
+# config valid only for current version of Capistrano
+lock '3.6.0'
 
-# This adds a task that precompiles assets for the asset pipeline
-load 'deploy/assets'
+set :application, 'planningalerts'
+set :repo_url, 'git://github.com/crowbot/planningalerts-app.git'
 
-set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
-before 'deploy', 'rvm:create_alias'
-before 'deploy', 'rvm:create_wrappers'
+# Default branch is :master
+set :branch, 'southwark'
 
-set :application, "planningalerts.org.au/app"
-set :repository,  "git://github.com/openaustralia/planningalerts-app.git"
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/srv/www/planningalerts'
 
-server "kedumba.openaustraliafoundation.org.au", :app, :web, :db, :primary => true
+# Default value for :scm is :git
+# set :scm, :git
 
-set :use_sudo, false
-set :user, "deploy"
-set :scm, :git
-set :stage, "test" unless exists? :stage
-set :rails_env, "production" #added for delayed job
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
-if stage == "production"
-  set :deploy_to, "/srv/www/www.#{application}"
-elsif stage == "test"
-  set :deploy_to, "/srv/www/test.#{application}"
-  #set :branch, "test"
-end
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
-# We need to run this after our collector mongrels are up and running
-# This goes out even if the deploy fails, sadly
-after "deploy:update", "newrelic:notice_deployment"
+# Default value for :pty is false
+# set :pty, true
 
-before "deploy:restart", "foreman:restart"
+# Default value for :linked_files is []
+append :linked_files, 'config/database.yml', 'config/general.yml'
 
-namespace :deploy do
-  desc "After a code update, we link additional config and the scrapers"
-  before "deploy:assets:precompile" do
-    links = {
-            "#{release_path}/config/database.yml"               => "#{shared_path}/database.yml",
-            "#{release_path}/config/throttling.yml"             => "#{shared_path}/throttling.yml",
-            "#{release_path}/app/models/configuration.rb"       => "#{shared_path}/configuration.rb",
-            "#{release_path}/config/production.sphinx.conf"     => "#{shared_path}/production.sphinx.conf",
-            "#{release_path}/config/sphinx.yml"                 => "#{shared_path}/sphinx.yml",
-            "#{release_path}/public/sitemap.xml"                => "#{shared_path}/sitemap.xml",
-            "#{release_path}/public/sitemaps"                   => "#{shared_path}/sitemaps",
-            "#{release_path}/public/assets"                     => "#{shared_path}/assets",
-    }
+# Default value for linked_dirs is []
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
-    # "ln -sf <a> <b>" creates a symbolic link but deletes <b> if it already exists
-    run links.map {|a| "ln -sf #{a.last} #{a.first}"}.join(";")
-  end
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-  task :restart, :except => { :no_release => true } do
-    run "touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
-
-namespace :foreman do
-  desc "Export the Procfile to Ubuntu's upstart scripts"
-  task :export, roles: :app do
-    run "cd #{current_path} && sudo bundle exec foreman export upstart /etc/init -u deploy -a planningalerts -f Procfile.production -l /srv/www/www.planningalerts.org.au/log --root /srv/www/www.planningalerts.org.au/app/current"
-  end
-
-  desc "Start the application services"
-  task :start, roles: :app do
-    sudo "service planningalerts start"
-  end
-
-  desc "Stop the application services"
-  task :stop, roles: :app do
-    sudo "service planningalerts stop"
-  end
-
-  desc "Restart the application services"
-  task :restart, roles: :app do
-    run "sudo service planningalerts restart"
-  end
-end
+# Default value for keep_releases is 5
+# set :keep_releases, 5
