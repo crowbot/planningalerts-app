948eb3f23a9c2521df239120a3f04a9225422f60
Louise Crow <louise.crow@gmail.com>
2016-08-07 14:51:13 +0100

    Add sphinx config file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,10 @@ set :deploy_to, '/srv/www/planningalerts'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/general.yml', 'config/test.yml'
+append :linked_files, 'config/database.yml',
+                      'config/general.yml',
+                      'config/test.yml',
+                      'config/production.sphinx.conf'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
