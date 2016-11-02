d0f4e0611868e04f258b0b2621184ec14e96849f
Louise Crow <louise.crow@gmail.com>
2016-11-02 14:11:28 +0000

    Use database-independent query.

--- spec/controllers/authorities_controller_spec.rb
+++ spec/controllers/authorities_controller_spec.rb
@@ -7,5 +7,15 @@ describe AuthoritiesController do
 
   describe "#index" do
     it { expect(get(:index)).to be_success }
+
+    it 'assigns a mapping of states to authorities to the view' do
+      nsw_auth = create(:authority, state: 'NSW')
+      vic_auth = create(:authority, state: 'VIC')
+
+      get :index
+      expect(assigns[:authorities]).to eq({'NSW' => [nsw_auth],
+                                           'VIC' => [vic_auth]})
+    end
+
   end
 end
